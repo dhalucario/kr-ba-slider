46f4896e22090a9092f48c555bae6e286d149cbb
Leon Grünewald <leon.gruenewald@kreativrudel.de>
2021-05-18 15:36:55 +0200

    Add scroll and events

--- src/kr-ba-slider.js
+++ src/kr-ba-slider.js
@@ -1,12 +1,14 @@
-export default class KrBaSlider {
+export default class KrBaSlider extends EventTarget {
     constructor(elem, config = {}) {
+        super();
+
         if (!elem instanceof HTMLElement) {
             throw new Error('First element parameter is invalid');
         }
 
         config = {...{
-            orientation: 'horizontal'
-        }, ...config};
+                orientation: 'horizontal'
+            }, ...config};
 
         this.baseElem = elem;
         this.baseElem.classList.add('krba-slider', 'krba-' + config.orientation);
@@ -19,6 +21,7 @@ export default class KrBaSlider {
         this.knob = elem.querySelector('.krba-knob');
         this.afterImageWidth = 0;
         this.knobIsHeld = false;
+        this.lastTouch = null;
         this.afterImageResObs = new ResizeObserver(() => {
             this.onBaseImageChange();
         });
@@ -27,9 +30,14 @@ export default class KrBaSlider {
         if (this.beforeImage === null) throw new Error('No before image is defined');
         if (this.afterImage === null) throw new Error('No after image is defined');
 
-        this.knob.addEventListener('mousedown', this.onKnobHold.bind(this), {});
+        this.knob.addEventListener('mousedown', this.onKnobHold.bind(this));
+        this.knob.addEventListener('touchstart', this.onKnobTouchHold.bind(this));
+
         document.addEventListener('mouseup', this.onKnobRelease.bind(this));
+        document.addEventListener('touchend', this.onKnobRelease.bind(this));
+
         document.addEventListener('mousemove', this.onKnobMove.bind(this));
+        this.baseElem.addEventListener('touchmove', this.onKnobTouchMove.bind(this));
     }
 
     onBaseImageChange() {
@@ -45,22 +53,61 @@ export default class KrBaSlider {
 
     onKnobRelease() {
         this.knobIsHeld = false;
+        this.dispatchEvent(new CustomEvent('knobrelease', {
+            detail: {
+                target: this,
+            }
+        }));
     }
 
     onKnobHold(e) {
+        this.knobIsHeld = true;
+        this.dispatchEvent(new CustomEvent('knobhold', {
+            detail: {
+                target: this,
+            }
+        }));
+    }
+
+    onKnobTouchHold(e) {
+        if (e.touches.length !== 1) return;
+
+        this.lastTouch = e.touches[0];
+
         e.preventDefault();
         e.stopPropagation();
         this.knobIsHeld = true;
+
+        this.dispatchEvent(new CustomEvent('knobhold', {
+            detail: {
+                target: this,
+            }
+        }));
     }
 
     onKnobMove(e) {
+        if (!this.knobIsHeld) return;
+
         e.preventDefault();
-        if (this.knobIsHeld) {
-            let nextPos = (parseInt(getComputedStyle(this.knob).left) + e.movementX);
-            if (nextPos < 0) nextPos = 0;
-            if (nextPos > this.afterImageWidth) nextPos = this.afterImageWidth;
-            this.knob.style.left = nextPos + 'px';
-            this.beforeWrapper.style.width = nextPos + 'px';
-        }
+        let nextPos = (parseInt(getComputedStyle(this.knob).left) + e.movementX);
+        if (nextPos < 0) nextPos = 0;
+        if (nextPos > this.afterImageWidth) nextPos = this.afterImageWidth;
+        this.knob.style.left = nextPos + 'px';
+        this.beforeWrapper.style.width = nextPos + 'px';
+    }
+
+    onKnobTouchMove(e) {
+        if (e.touches.length !== 1) return;
+        if (!this.knobIsHeld) return;
+        e.preventDefault();
+
+        let movementX = (this.lastTouch.clientX - e.touches[0].clientX)*-1;
+        this.lastTouch = e.touches[0];
+
+        let nextPos = (parseInt(getComputedStyle(this.knob).left) + movementX);
+        if (nextPos < 0) nextPos = 0;
+        if (nextPos > this.afterImageWidth) nextPos = this.afterImageWidth;
+        this.knob.style.left = nextPos + 'px';
+        this.beforeWrapper.style.width = nextPos + 'px';
     }
 }
